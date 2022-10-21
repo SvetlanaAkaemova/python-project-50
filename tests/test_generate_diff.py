3059c104f894cef58237e872bd41bb68d1979095
svetlana_akaemova <akaemova.sv@mail.ru>
2022-10-21 16:08:23 +0200

    fixed tests after linter

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,16 +1,15 @@
-import ast
 import pytest
 from gendiff import generate_diff
 
 
 parameter = [('path1_plain_json', 'path2_plain_json', 'stylish', 'tests/fixtures/result_example.txt'),
-    ('path1_plain_yml', 'path2_plain_yml', 'stylish', 'tests/fixtures/result_example.txt'),
-    ('path1_json', 'path2_json', 'stylish', 'tests/fixtures/result_stylish.txt'),
-    ('path1_json', 'path2_json', 'plain', 'tests/fixtures/result_plain.txt'),
-    ('path1_json', 'path2_json', 'json', 'tests/fixtures/result_json.txt'),
-    ('path1_yml', 'path2_yml', 'stylish', 'tests/fixtures/result_stylish.txt'), 
-    ('path1_yml', 'path2_yml', 'plain', 'tests/fixtures/result_plain.txt'),
-    ('path1_yml', 'path2_yml', 'json', 'tests/fixtures/result_json.txt'),
+             ('path1_plain_yml', 'path2_plain_yml', 'stylish', 'tests/fixtures/result_example.txt'),
+             ('path1_json', 'path2_json', 'stylish', 'tests/fixtures/result_stylish.txt'),
+             ('path1_json', 'path2_json', 'plain', 'tests/fixtures/result_plain.txt'),
+             ('path1_json', 'path2_json', 'json', 'tests/fixtures/result_json.txt'),
+             ('path1_yml', 'path2_yml', 'stylish', 'tests/fixtures/result_stylish.txt'),
+             ('path1_yml', 'path2_yml', 'plain', 'tests/fixtures/result_plain.txt'),
+             ('path1_yml', 'path2_yml', 'json', 'tests/fixtures/result_json.txt'),
 ]
 
 
